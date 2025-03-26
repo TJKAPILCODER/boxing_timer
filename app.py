b98ade0ad8a16c5f432241c5c88b5b472286e047
TJ Kapil <tjkapil1998@gmail.com>
2025-03-26 11:29:46 -0700

    Changes made to app.py, added a route in app.py for timer.html page

---
+++ app.py
@@ -0,0 +1,20 @@
+# Author: TJ Kapil
+# Project: Boxing Timer
+# INFO: The app.py file will contain the Python Flash code. 
+
+from flask import Flask, render_template # This line imports the flask class from the Flask Module/Library. 
+
+app = Flask(__name__) # This line creates an instance of the Flask Class and assigns it to the variable app. 
+
+@app.route("/") # This is a decorater that tells Flask which URL should trigger the hello() function, in this case, it's the root URL. 
+# Root is always "/"
+# A decorater is a way to modify or extend the behavior of a function. 
+def hello(): # This is a function named hello(), which will executed when the user visits the root url. 
+    return "Hello, World!" # This line retuns the string  "Hello, World!" as a response to the user's browser. Flask will send this string
+                           # as the HTML content of the page. 
+@app.route("/timer") # This will add a route to the timer.html file. 
+def show_timer():
+    return render_template("timer.html") # This will render the timer.html pahge
+
+if __name__ == "__main__":
+    app.run(debug=True)
